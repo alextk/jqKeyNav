4dcd9fd80ea3b0cdedee58e2e3ebcc641d279fd3
Alex <tkachev.alex@gmail.com>
2011-12-13 21:37:37 +0200

    use rake build system with rego-js-builder gem

--- Rakefile
+++ Rakefile
@@ -1,9 +1,24 @@
+require 'rubygems'
+gem 'rego-ruby-ext'
+require "rego-ruby-ext"
+gem 'rego-js-builder'
+require "rego-js-builder"
+gem 'rake-hooks'
+require 'rake/hooks'
 
+project = JsProjectBuilder.new(
+  :name => 'jqKeyNav',
+  :description => 'jQuery plugin for keyboard navigation events binding',
+  :file_name => 'jquery.keynav.js',
+  :js_files => %w{keynav.js}
+)
+JsProjectBuilder::Tasks.new(project)
+
+
+# for spec testing
 begin
   require 'jasmine'
   load 'jasmine/tasks/jasmine.rake'
 rescue LoadError
-  task :jasmine do
-    abort "Jasmine is not available. In order to run jasmine, you must: (sudo) gem install jasmine"
-  end
+  puts "---\nERROR: Jasmine is not available. In order to run jasmine, you must: (sudo) gem install jasmine\n---\n"
 end
--- dist/jquery.keynav.pack.js
+++ dist/jquery.keynav.pack.js
@@ -2,14 +2,14 @@
 * jqKeyNav - jQuery plugin for keyboard navigation events binding
 *
 * Version: 0.0.1
-* Build: 4
+* Build: 8
 * Copyright 2011 Alex Tkachev
 *
 * Dual licensed under MIT or GPLv2 licenses
 *   http://en.wikipedia.org/wiki/MIT_License
 *   http://en.wikipedia.org/wiki/GNU_General_Public_License
 *
-* Date: 30/09/2011 11:52:00
+* Date: 13 Dec 2011 21:33:27
 */
 
 eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('(1(a){4 b={W:"C",14:"p",16:"x",Q:"q",10:"t",11:"u",J:"m",N:"v",I:"n",13:"o",15:"G",9:"z"},c=1(){2.y.F(2,7)};a.L(c.M,{y:1(b){b=b||{},2.l=b.O||b.l||P;4 c={};["o","C","p","x","q","z","G","t","u","m","v","n"].T(1(c){k a.U(b[c])}).w(1(a){c[a]=b[a]}),2.j=c},i:1(a){4 c=a.5.E,d=a.V,e=b[d];B(e&&c.j[e]){4 f=c.j[e],g=c.l||f;f.Z(g,a)!==!0&&a.H()}},8:1(){2.6.F(2,7);D(4 a=0;a<7.A;a++){4 b=7[a];b r h&&b.8("s",{E:2},2.i)}},6:1(){D(4 a=0;a<7.A;a++){4 b=7[a];b r h&&b.6("s",2.i)}}}),a.S.3=1(b){k 2.w(1(){4 c=a(2);B(a.X(b)){4 d=a.3(b);c.5("3")&&c.5("3").6(),c.5("3",d),d.8(c)}K b=="8"?c.5("3").8(c):b=="6"?c.5("3").6(c):b=="R"&&(c.5("3").6(c),c.12("3"))})},a.3=1(a){k Y c(a)}})(h)',62,69,'|function|this|keyNav|var|data|unbind|arguments|bind|||||||||jQuery|relay|handlers|return|scope|del|end|enter|right|down|instanceof|keydown|pageUp|pageDown|home|each|up|initialize|tab|length|if|left|for|keynav|apply|esc|stopEvent|35|46|else|extend|prototype|36|context|null|40|destroy|fn|select|isFunction|which|37|isPlainObject|new|call|33|34|removeData||39|27|38'.split('|'),0,{}))
